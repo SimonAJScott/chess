f7acbdf001e16328d684fa6169ee49b0e96acb2d
SimonAJScott <simon.aj.scott@gmail.com>
2025-05-10 19:09:21 -0400

    progress

--- Board.py
+++ Board.py
@@ -7,7 +7,13 @@ class Board:
     def pixelToArray(self, mouse_pos):
         row = (mouse_pos[0]-30)//78.75
         col = (mouse_pos[1]-30)//78.75
-        return row, col
+        return (row, col)
 
     def arrayToPixel(self, row, col):
-        return 0
+        # row + size of each square + margin + in the center of square + in center of the image (20*20)
+        height = (row)*78.75+30+78.75/2
+        width = (col)*78.75+30+78.75/2
+        return (height, width)
+
+    def getBoard(self):
+        return self.board
--- main.py
+++ main.py
@@ -6,11 +6,13 @@ pygame.init()
 
 board = pygame.image.load("assets/board.jpg")
 icon = pygame.image.load("assets/icon.png")
+
 board_width, board_height = board.get_size()
 screen = pygame.display.set_mode((board_width, board_height))
 pygame.display.set_icon(icon)
 pygame.display.set_caption("simon's chess game!")
-
+greycircle = pygame.image.load("assets/greycircle.png")
+resized_image = pygame.transform.scale(greycircle, (20, 20))
 # Main loop
 running = True
 while running:
@@ -21,9 +23,11 @@ while running:
             if (mouse_pos[0] <= 30 or mouse_pos[0] >= 660 or mouse_pos[1] <= 30 or mouse_pos[1] >= 660):
                 print("out of bounds")
             else:
-                # print(f"Mouse clicked at: {mouse_pos}")
+                print(f"Mouse clicked at: {mouse_pos}")
+                locationInArray = Board.pixelToArray(Board, mouse_pos)
+                print(f"in the array at: {locationInArray}")
                 print(
-                    f"in the array at: {Board.pixelToArray(Board, mouse_pos)}")
+                    f"calculated pixel location: {Board.arrayToPixel(Board, locationInArray[0], locationInArray[1])}")
         if event.type == pygame.QUIT:
             running = False
 
@@ -33,6 +37,11 @@ while running:
     # Draw the image at (0, 0)
     screen.blit(board, (0, 0))
 
+    for i in range(0, 8):
+        for j in range(0, 8):
+            screen.blit(resized_image, tuple(
+                # subtract 10 from each (icon is 20x20)
+                x - 10 for x in Board.arrayToPixel(Board, i, j)))
     # Update the display
     pygame.display.flip()
 
